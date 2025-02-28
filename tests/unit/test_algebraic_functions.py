ddff00420e630d0bd715c7b8f0be79e8f13ec5d0
Asser OMAR <asserrefaat03@gmail.com>
2025-02-28 15:00:10 +0100

    Initial commit for Lab 3

---
+++ tests/unit/test_algebraic_functions.py
@@ -0,0 +1,28 @@
+""" 
+Unit tests for the calculator library 
+""" 
+ 
+import os, sys 
+ 
+# Add project root to Python path 
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")) 
+sys.path.insert(0, project_root) 
+ 
+from src.utils import add, sub, mul, div 
+ 
+class TestCalculator: 
+ 
+    def test_addition(self): 
+        assert 4 == add(2, 2) 
+ 
+
+    def test_subtraction(self): 
+        assert 2 == sub(4, 2)
+
+
+    def test_multiplication(self):
+        assert 6 == mul(3, 2)
+
+
+    def test_division(self):
+        assert 6 == div(12, 2)
